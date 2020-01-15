9fb28d45246e4b38e67435e809b4027552ea1122
github username <github email address>
2020-01-15 17:40:50 +0000

    added basic user table

---
+++ db/migrate/20200115173959_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.float :weight
+      t.float :height
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
