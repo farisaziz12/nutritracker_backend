ba82d52460abab102dec1cbd4146fd6ef29a7fa2
Faris Aziz <farisaziz12@gmail.com>
2020-01-16 17:13:47 +0000

    api request functioning properly for food requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'foods/index'
   get 'foods/show'
+  get '/search' => 'foods#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
