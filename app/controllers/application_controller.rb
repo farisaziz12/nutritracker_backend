d5e72f5c2f20484f1127e3edd165d37a4c2dd11c
github username <github email address>
2020-01-16 15:11:05 +0000

    working on serialization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  def auth_header
+    request.headers["Authorization"] || request.headers["Authorisation"]
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def require_login
+    render json: {message: "You need to be logged in to see this page"} if !logged_in?
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
   def token
     JWT.encode({user_id: self.id}, 'secret')
