8fe599c3d24d71cb84267763ae5b246634070661
github username <github email address>
2020-01-17 10:14:28 +0000

    added name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
-
+  validates :name, presence: true
   def token
     JWT.encode({user_id: self.id}, ENV["NUTRI"])
   end
